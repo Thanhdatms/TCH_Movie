a973ad5e0804c291649769aaeba71fcb82ef340e
Nguynchau150903 <21521886@gm.uit.edu.vn>
2024-11-22 13:40:39 +0700

    feat: watch page, update: home screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { LogOut, Menu, Search } from "lucide-react";
 import { useAuthStore } from "../store/authUser";
+import { useContentStore } from "../store/content";
 
 const Navbar = () => {
 	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -9,6 +10,7 @@ const Navbar = () => {
 
 	const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
 
+	const { setContentType } = useContentStore();
 
 	return (
 		<header className='max-w-6xl mx-auto flex flex-wrap items-center justify-between p-4 h-20'>
@@ -19,10 +21,10 @@ const Navbar = () => {
 
 				{/* desktop navbar items */}
 				<div className='hidden sm:flex gap-2 items-center'>
-					<Link to='/' className='hover:underline'>
+					<Link to='/' className='hover:underline' onClick={() => setContentType("movie")}>
 						Movies
 					</Link>
-					<Link to='/' className='hover:underline'>
+					<Link to='/' className='hover:underline' onClick={() => setContentType("tv")}>
 						Tv Shows
 					</Link>
 					<Link to='/history' className='hover:underline'>
