e15474ef7047eedef1648642409053e610a34230
Nguynchau150903 <21521886@gm.uit.edu.vn>
2024-11-22 14:02:28 +0700

    feat: 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import { useEffect } from "react";
 import { Loader } from "lucide-react";
 import SearchPage from "./pages/SearchPage";
 import SearchHistoryPage from "./pages/SearchHistoryPage";
+import NotFoundPage from "./pages/404";
 
 function App() {
 	const { user, isCheckingAuth, authCheck } = useAuthStore();
@@ -37,6 +38,7 @@ function App() {
 				<Route path='/watch/:id' element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
 				<Route path='/search' element={user ? <SearchPage /> : <Navigate to={"/login"} />} />
 				<Route path='/history' element={user ? <SearchHistoryPage /> : <Navigate to={"/login"} />} />
+				<Route path='/*' element={<NotFoundPage />} />
 			</Routes>
 			<Footer />
 
